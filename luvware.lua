477ea8fbdcbca15c79535b6e66ebb5008a8bafa6
eliriggs <eliriggs@cock.li>
2015-03-16 13:47:48 -0500

    added sql store

--- luvware.lua
+++ luvware.lua
@@ -53,6 +53,30 @@ end
 
 local function wrapSession(options)
 	local hex = '0123456789ABCDEF'
+	local store = options.store or 'memory'
+	local sessionStore
+	local db
+
+	local stmtQuerySession
+
+	if store == 'memory' then
+		sessionStore = {}
+	else
+		sqlite3 = require('ljsqlite3')
+		db = sqlite3.open('sessions.db')
+		db:exec[[
+		create table if not exists sessions (
+		id integer primary key,
+		sessionid varchar,
+		sessionkey varchar,
+		sessionval varchar
+		);
+		]]
+		stmtQuerySession = db:prepare[[
+		select sessionval from sessions where sessionid = ? and sessionkey = ?;
+		]]
+	end
+
 	local function int2hex(n)
 		local s = ''
 		local b = 16
@@ -65,26 +89,71 @@ local function wrapSession(options)
 		while #s < 2 do s = '0' .. s end
 		return s
 	end
+
 	local function genID()
 		return int2hex(math.random(0, 255)) ..
-					 int2hex(math.random(0, 255)) ..
-					 int2hex(math.random(0, 255)) ..
-					 int2hex(math.random(0, 255))
+		int2hex(math.random(0, 255)) ..
+		int2hex(math.random(0, 255)) ..
+		int2hex(math.random(0, 255))
 	end
+
+	local function getSessionValue(id)
+		local function inner(t, k)
+			if store == 'memory' then
+				return sessionStore[id][k]
+			elseif store == 'sqlite' then
+				stmtQuerySession:bind1(1, id)
+				stmtQuerySession:bind1(2, k)
+				resultset, nrow = stmtQuerySession:resultset()
+				stmtQuerySession:reset()
+				if nrow == 0 then
+					return nil
+				elseif nrow == 1 then
+					-- first row, second value returned (sessionval)
+					return resultset[1][1]
+				else
+					print('error! sqlite: sessionid '..id..' has '..nrow..' values for '..k)
+					return nil
+				end
+			end
+		end
+		return inner
+	end
+
+	local function setSessionValue(id)
+		local function inner(t, k, v)
+			if store == 'memory' then
+				sessionStore[id][k] = v
+			elseif store == 'sqlite' then
+				db:exec([[
+				insert or replace into 
+				sessions(id, sessionid, sessionkey, sessionval) 
+				values(
+				(select id from sessions where sessionid = ']]..id.."' and sessionkey = '"..k..[[')
+				, ']]..id.."' ,'"..k.."' ,'"..v.."');")
+			end
+		end
+		return inner
+	end
+
 	local function inner(req)
 		local cookie = req.headers.cookie
+		local id
 		if cookie then
 			local s = {}
 			for k, v in cookie:gmatch('[^=]+') do
 				table.insert(s, k)
 			end
-			req.session = s[2]
+			id = s[2]
 		else
-			local id = genID()
+			id = genID()
 			req.response:setHeader('Set-Cookie', 'session=' .. id)
-			req.session = id
 		end
+		-- the browser can have a cookie but there isn't a store
+		if store == 'memory' and not sessionStore[id] then sessionStore[id] = {} end
+		req.session = setmetatable({}, {__index=getSessionValue(id), __newindex=setSessionValue(id)})
 	end
+
 	return inner
 end
 
